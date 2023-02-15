25de15aa350cebb0ff3a30ff6f7f516ee2bc1bf9
Vyacheslav <astapovich_work@gmail.com>
2023-02-15 13:17:34 +0300

    added account, bill, card, currency and user classes

--- src/main/java/com/solvd/atm/models/Account.java
+++ src/main/java/com/solvd/atm/models/Account.java
@@ -1,4 +1,39 @@
 package com.solvd.atm.models;
 
+import java.util.Date;
+
 public class Account {
+    private Date openingDate;
+    private float totalSum;
+    private Currency currency;
+    private User owner;
+
+    public Date getOpeningDate() {
+        return openingDate;
+    }
+
+    public float getTotalSum() {
+        return totalSum;
+    }
+
+    public void setTotalSum(float totalSum) {
+        this.totalSum = totalSum;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.totalSum = currency.fromOldCourse(totalSum, this.currency.getCourse());
+        this.currency = currency;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/solvd/atm/models/Card.java
+++ src/main/java/com/solvd/atm/models/Card.java
@@ -1,4 +1,60 @@
 package com.solvd.atm.models;
 
+import java.util.Date;
+
 public class Card {
+    private String number;
+    private User owner;
+    private Account account;
+    private Date validityDate;
+    private String cvv;
+    private int pin;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public User owner() {
+        return owner;
+    }
+
+    public void setOwner(User holder) {
+        this.owner = holder;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Date getValidityDate() {
+        return validityDate;
+    }
+
+    public void setValidityDate(Date validityDate) {
+        this.validityDate = validityDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
 }
