4554a1f220b3910f89450e90cfef14cc8673d05b
maxza <maxzah99@gmail.com>
2023-02-15 14:20:37 +0300

    Add CArd Menu to work with cards

---
+++ src/main/java/com/solvd/atm/atm_menu/working_with_card/CardMenu.java
@@ -0,0 +1,41 @@
+package com.solvd.atm.atm_menu.working_with_card;
+
+import com.solvd.atm.Utils.MyLogger;
+import com.solvd.atm.models.Account;
+import com.solvd.atm.models.Card;
+
+import java.util.Scanner;
+
+import static com.solvd.atm.atm_menu.MainMenu.mainMenu;
+
+public class CardMenu {
+    private static final MyLogger LOGGER = MyLogger.getInstance();
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static boolean cardMenu(boolean exit, Card card) {
+        Account account = card.getAccount();
+        LOGGER.info("Welcome, choose what you want to do");
+        LOGGER.info("1. Withdraw money from account");
+        LOGGER.info("2. Add money to account");
+        LOGGER.info("3. Watch balance on account");
+        LOGGER.info("4. Transfer money to another account");
+        LOGGER.info("5. Pay bill");
+        LOGGER.info("6. Exit to menu");
+        LOGGER.info("7. Take off card");
+        int point = SCANNER.nextInt();
+        switch (point) {
+            case 1 -> WithdrawMenu.withdrawMoneyFromAccount(account);
+            case 2 -> AddingMoneyMenu.addMoneyToAccount(account);
+            case 3 -> AccountInfoMenu.getMoneyInfo(account);
+            case 4 -> TransferMenu.transferMoneyFromAccount(account);
+            case 5 -> BillsMenu.payBills(account);
+            case 6 -> mainMenu(false);
+            case 7 -> exit = true;
+            default -> {
+                LOGGER.info("You choose invalid point, try again :(");
+                cardMenu(exit, card);
+            }
+        }
+        return exit;
+    }
+}
