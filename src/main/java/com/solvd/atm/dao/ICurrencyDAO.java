3963cf50f5fc6b6dddc92cb231e12431198077ce
maxza <maxzah99@gmail.com>
2023-02-21 00:21:10 +0300

    fixed exchange

--- src/main/java/com/solvd/atm/dao/ICurrencyDAO.java
+++ src/main/java/com/solvd/atm/dao/ICurrencyDAO.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.*;
 import java.util.List;
 
 public interface ICurrencyDAO extends IBaseDAO<Currency> {
-    @Select("SELECT course, id_currency FROM currency WHERE currency_name = #{currency_name}")
+    @Select("SELECT course, id_currency FROM currency WHERE name_currency = #{currency_name}")
     @Results(value = {
             @Result(property = "idCurrency", column = "id_currency"),
             @Result(property = "course", column = "course")
@@ -16,11 +16,11 @@ public interface ICurrencyDAO extends IBaseDAO<Currency> {
     @Update("UPDATE currency SET course = #{course} WHERE id_currency = #{idCurrency}")
     boolean updateEntity(Currency entity);
 
-    @Select("SELECT course, id_currency, currency_name FROM currency")
+    @Select("SELECT course, id_currency, name_currency FROM currency")
     @Results(value = {
             @Result(property = "idCurrency", column = "id_currency"),
             @Result(property = "course", column = "course"),
-            @Result(property = "name", column = "currency_name")
+            @Result(property = "name", column = "name_currency")
     })
     List<Currency> getAllCurrency();
 }
